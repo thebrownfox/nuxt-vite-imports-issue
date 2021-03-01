3c4137bb9462f1629cfd1092644af09aed0de0d1
Marek Brenčič <marek.brencic.sxe@gmail.com>
2021-03-01 19:31:08 +0100

    Initial commit

---
+++ components/NanoId.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container">
+    <div>
+      <h1>
+        Hello from <span class="name">{{ id }}</span
+        >.
+      </h1>
+    </div>
+  </div>
+</template>
+
+<script>
+import { nanoid } from "nanoid";
+
+export default {
+  data() {
+    return {
+      id: nanoid(8),
+    };
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+h1 {
+  font-weight: 400;
+}
+.name {
+  color: #00C48D;
+}
+p {
+  margin-top: 20px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <section>
+    <div>
+      <Logo :width="350" />
+      <h1 class="title">NUXT<span class="green">JS</span></h1>
+      <h2 class="subtitle">Starter for CodeSandBox</h2>
+    </div>
+    <NanoId></NanoId>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  display: block;
+  font-weight: 400;
+  font-size: 100px;
+  color: #2E495E;
+  letter-spacing: 1px;
+  font-size: 6em;
+}
+.green {
+  color: #00C48D;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 3em;
+  color: #2E495E;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
